e51658e5b45594c4c28931e69b721f0d31cd5317
Luis Angel Garcia <luis.a.garcia01996@gmail.com>
2017-04-26 23:07:24 -0500

    Updated Animatio Test
    TODO:
    FIgure out why animation is not working, or use the other animation tutorial...

---
+++ AllSources/AnimatioTest2/Mesh.cpp
@@ -0,0 +1,112 @@
+#include "Mesh.h"
+#include "Model.h"
+
+void Mesh::_SetUpMesh()
+{
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, ListVertices.size() * sizeof(Vertex), &ListVertices[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+
+	//Vertex Positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)0);
+
+	//Vertex Normals
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Normal));
+
+	//Texture Coordinates
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, TexCoords));
+
+	//Vector IDs
+	glEnableVertexAttribArray(3);
+	glVertexAttribIPointer(3, 1, GL_INT, sizeof(Vertex), (GLvoid*)offsetof(Vertex, id));
+
+	//Vector Weights
+	glEnableVertexAttribArray(4);
+	glVertexAttribPointer(4, 4, GL_FLOAT, GL_TRUE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Weights));
+
+
+	glBindVertexArray(0);
+}
+
+void Mesh::Draw(Shader shader, glm::vec3 Color, float shininess)
+{
+	GLuint diffuse_Num = 1;
+	GLuint specular_Num = 1;
+	if (this->ListTextures.size() == 0)
+	{
+		glUniform1i(glGetUniformLocation(shader.Program, "accessTextures"), 0);
+		glUniform3f(glGetUniformLocation(shader.Program, "material.defaultDiffuse"), 0.3f, 0.3f, 0.3f);
+	}
+	else
+	{
+		glUniform1i(glGetUniformLocation(shader.Program, "accessTextures"), 1);
+		for (int i = 0; i < this->ListTextures.size(); i++)
+		{
+			glActiveTexture(GL_TEXTURE0 + i);
+			std::stringstream ss;
+			std::string number;
+			std::string name = ListTextures[i].type;
+
+			if (this->ListTextures[i].type == "texture_diffuse")
+			{
+				ss << diffuse_Num++;
+			}
+			if (this->ListTextures[i].type == "texture_specular")
+			{
+				ss << specular_Num++;
+			}
+
+			number = ss.str();
+
+			glUniform1i(glGetUniformLocation(shader.Program, ("material." + name + number).c_str()), i);
+			glBindTexture(GL_TEXTURE_2D, this->ListTextures[i].id); //Bind the texture
+		}
+	}
+
+	glActiveTexture(GL_TEXTURE0);
+
+	glUniform1f(glGetUniformLocation(shader.Program, "material.shininess"), shininess);
+
+	glUniform3f(glGetUniformLocation(shader.Program, "material.ambient"), Color.x, Color.y, Color.z);
+
+	//Load the bone matrices if the model is rigged
+	if (Model_I_Belong_To->is_rigged)
+	{
+		/*
+		for (int i = 0; i < Model_I_Belong_To->GetSkeleton()->BoneMatrices.size(); i++)
+		{
+			std::cout << "[\n " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[0][0] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[0][1] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[0][2] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[0][3] << std::endl
+				<< Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[1][0] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[1][1] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[1][2] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[1][3] << std::endl
+				<< Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[2][0] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[2][1] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[2][2] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[2][3] << std::endl
+				<< Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[3][0] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[3][1] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[3][2] << ", " << Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(i)[3][3] << std::endl
+				<< "]" << std::endl;
+		}
+		*/
+		glUniformMatrix4fv(glGetUniformLocation(shader.Program, "mBones"),
+			Model_I_Belong_To->GetSkeleton()->BoneMatrices.size(), GL_FALSE,
+			glm::value_ptr(Model_I_Belong_To->GetSkeleton()->BoneMatrices.at(0)));
+	}
+		
+
+	//Draw the mesh with textures and shading and all that beautiful glory
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+
+	for (GLuint i = 0; i < this->ListTextures.size(); i++)
+	{
+		glActiveTexture(GL_TEXTURE0 + i);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+}
---
+++ AllSources/AnimatioTest2/Skeleton.h
@@ -0,0 +1,179 @@
+#pragma once
+#ifndef SKELETON_H
+#define SKELETON_H
+#include "MatrixConversions.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include<assimp\Importer.hpp>
+#include<assimp\scene.h>
+#include<assimp\postprocess.h>
+
+#include<map>
+#include<vector>
+#include<string>
+
+#include "Bone.h"
+
+class Skeleton
+{
+public:
+	std::vector<Bone*> List_Bones;
+	glm::mat4 globalInverseTransform;
+	std::vector<glm::mat4> BoneMatrices;
+
+	float time;
+	float start_time;
+	float end_time;
+
+	Animation * active_animation;
+	Animation * idle_animation;
+
+	bool anim_play;
+	bool anim_loop;
+
+	Skeleton()
+	{
+		this->globalInverseTransform = glm::mat4();
+
+		this->time = this->start_time = this->end_time;
+		this->active_animation = nullptr;
+		this->idle_animation = nullptr;
+
+		anim_play = false;
+		anim_loop = false;
+	}
+
+	Skeleton(std::vector<Bone*> in_bones, glm::mat4 in_globalInverseTransform)
+	{
+		Init(in_bones, in_globalInverseTransform);
+	}
+
+	void Init(std::vector<Bone*> in_bones, glm::mat4 in_globalInverseTransform)
+	{
+		List_Bones = in_bones;
+		this->globalInverseTransform = in_globalInverseTransform;
+
+		for (int i = 0; i < List_Bones.size(); i++)
+		{
+			List_Bones.at(i)->parent_skeleton = this;
+		}
+	}
+
+	Bone* FindBone(std::string name)
+	{
+		for (int i = 0; i < List_Bones.size(); i++)
+		{
+			if (List_Bones.at(i)->name == name)
+			{
+				return List_Bones.at(i);
+			}
+		}
+		return nullptr;
+	}
+
+	//Gather all of the bone's global transformations and place them in order in a 4x4 matrix
+	void UpdateBoneMatsVector()
+	{
+		if (List_Bones.size() == 0)
+			return;
+
+		//Clear the bone transformation vector so we don't have any weird artifacts.
+		BoneMatrices.clear();
+
+		int i = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			//Pass the identity matrix if we are past the number of bones in the actual skeleton
+			if (i > List_Bones.size() - 1)
+			{
+				BoneMatrices.push_back(glm::mat4(1.0f));
+			}
+			else
+			{
+				std::cout << "[\n " << List_Bones.at(i)->offset_matrix[0][0] << ", " << List_Bones.at(i)->offset_matrix[0][1] << ", " << List_Bones.at(i)->offset_matrix[0][2] << ", " << List_Bones.at(i)->offset_matrix[0][3] << std::endl
+					<< List_Bones.at(i)->offset_matrix[1][0] << ", " << List_Bones.at(i)->offset_matrix[1][2] << ", " << List_Bones.at(i)->offset_matrix[1][3] << std::endl
+					<< List_Bones.at(i)->offset_matrix[2][0] << ", " << List_Bones.at(i)->offset_matrix[2][1] << ", " << List_Bones.at(i)->offset_matrix[2][2] << ", " << List_Bones.at(i)->offset_matrix[2][3] << std::endl
+					<< List_Bones.at(i)->offset_matrix[3][0] << ", " << List_Bones.at(i)->offset_matrix[3][1] << ", " << List_Bones.at(i)->offset_matrix[3][2] << ", " << List_Bones.at(i)->offset_matrix[3][3] << std::endl
+					<< "]" << std::endl;
+				//If we are not past the number of bones in the skeleton,
+				//Multiply the concatenated transofmrs of the bone's parents with the bone's transformation.
+				//A note: the bone's matrix is NOT in world space. This is in local space. Conversion with world space
+				//will happen in the shader stage.
+				glm::mat4 concatenated_transformation = List_Bones.at(i)->GetParentTransforms() * AiToGLMMat4(List_Bones.at(i)->node->mTransformation);
+				//Transform the bone back to the origin by multiplying it with its offset matrix, then multiply it by the concatenated transforms,
+				//and then by the global inverse transform. Note that the multiplication happens backwards.
+				BoneMatrices.push_back(globalInverseTransform * concatenated_transformation * List_Bones.at(i)->offset_matrix);
+			}
+		}
+	}
+
+	void PlayAnimation(Animation * anim, bool loop, bool reset_to_start)
+	{
+		//If there's an animation currently playing
+		if (active_animation != nullptr)
+		{
+			//If the new animation is more important than the current animation, run the new animation
+			if (anim->priority < active_animation->priority)
+			{
+				active_animation = anim;
+			}
+			else
+				return;
+		}
+		else
+			active_animation = anim;
+
+		this->start_time = active_animation->start_time;
+		this->end_time = active_animation->end_time;
+		this->anim_play = true;
+		this->anim_loop = loop;
+
+		//If an animation should restart upon playing, reset it
+		if (reset_to_start)
+			this->time = active_animation->start_time;
+	}
+
+	void StopAnimation()
+	{
+		this->time = this->end_time;
+		this->active_animation = nullptr;
+		this->anim_play = false;
+	}
+
+	//Set the idle animation
+	void SetIdleAnimation(Animation* idle)
+	{
+		idle_animation = idle;
+	}
+
+	void Update(float deltaTime)
+	{
+		UpdateBoneMatsVector();
+
+		if (!anim_play)
+			return;
+
+		//Update the time variable by adding a delta time
+		this->time += deltaTime;
+
+		//Make sure that the time is not less than the current animation's start time
+		if (this->time < this->start_time)
+			this->time = this->start_time;
+
+		//The time cannot be greater than the animation's end time. If it is, and looping is not set, then loop the animation
+		if (this->time > this->end_time)
+		{
+			if (this->anim_loop)
+				this->time = this->start_time;
+			else
+				StopAnimation();
+		}
+
+		for (int i = 0; i < List_Bones.size(); i++)
+			List_Bones.at(i)->UpdateKeyframeTransform(this->time);
+	}
+};
+#endif
